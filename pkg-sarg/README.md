fa70b6d8a0bb2469134d0079067fe066070ccf91
Marcello Coutinho <marcello.coutinho@gmail.com>
2018-05-14 16:02:18 -0300

    The return of sarg package

---
+++ pkg-sarg/README.md
@@ -0,0 +1,8 @@
+# Sarg Unofficial packages for pfSense software
+
+The Sarg package will create graphical reports to squid/e2guardian proxy access.log
+
+# Install instructions
+
+If you enabled the Unofficial repo, you can add this package under System -> Package Manager
+
---
+++ pkg-sarg/files/usr/local/www/sarg_reports.php
@@ -0,0 +1,81 @@
+<?php
+/*
+	sarg_reports.php
+	part of pfSense (https://www.pfSense.org/)
+	Copyright (C) 2012 Marcello Coutinho <marcellocoutinho@gmail.com>
+	Copyright (C) 2015 ESF, LLC
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+require("guiconfig.inc");
+
+if ($savemsg) {
+    print_info_box($savemsg);
+}
+
+$pgtitle = array(gettext("Package"), gettext("Sarg"), gettext("Reports"));
+$shortcut_section = "sarg";
+include("head.inc");
+	
+?>
+<body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+<?php include("fbegin.inc"); ?>
+	
+<form>
+<div id="mainlevel">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+	<tr><td>
+		<?php
+		$tab_array = array();
+		$tab_array[] = array(gettext("General"), false, "/pkg_edit.php?xml=sarg.xml&id=0");
+		$tab_array[] = array(gettext("Users"), false, "/pkg_edit.php?xml=sarg_users.xml&id=0");
+		$tab_array[] = array(gettext("Schedule"), false, "/pkg.php?xml=sarg_schedule.xml");
+		$tab_array[] = array(gettext("View Report"), true, "/sarg_reports.php");
+		$tab_array[] = array(gettext("XMLRPC Sync"), false, "/pkg_edit.php?xml=sarg_sync.xml&id=0");
+		display_top_tabs($tab_array);
+		conf_mount_rw();
+		mwexec('/bin/rm -f /usr/local/www/sarg-images/temp/*');
+		conf_mount_ro();
+		?>
+	</td></tr>
+	<tr><td>
+		<div id="mainarea">
+		</div>
+		<br />
+		<script type="text/javascript">
+		//<![CDATA[
+		var axel = Math.random() + "";
+		var num = axel * 1000000000000000000;
+		document.writeln('<iframe src="/sarg_frame.php?prevent='+ num +'?"  frameborder="0" width="100%" height="600"></iframe>');
+		//]]>
+		</script>
+		<div id="file_div"></div>
+	</td></tr>
+</table>
+</div>
+</form>
+<?php 
+include("fend.inc");
+?>
+</body>
+</html>
