3a700d62306cbea3d09d88e508a1fb8641276f0c
Marcello Coutinho <marcellocoutinho@gmail.com>
2020-09-18 23:18:11 -0300

    Update postfix_queue.php

--- pkg-postfix/files/usr/local/www/postfix_queue.php
+++ pkg-postfix/files/usr/local/www/postfix_queue.php
@@ -3,7 +3,7 @@
  * postfix_queue.php
  *
  * part of Unofficial packages for pfSense(R) softwate
- * Copyright (c) 2011-2019 Marcello Coutinho
+ * Copyright (c) 2011-2020 Marcello Coutinho
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
